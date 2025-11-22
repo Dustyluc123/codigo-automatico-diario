3e5a71a72c41b870384c28679ba3eed566430b5f
Dustyluc123 <149323009+Dustyluc123@users.noreply.github.com>
2025-11-22 18:21:42 -0300

    Delete pedidos directory

--- pedidos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Produto, Pedido, ItemPedido
-
-# Registra seus modelos aqui para que eles apareçam no painel de administração.
-admin.site.register(Cliente)
-admin.site.register(Produto)
-admin.site.register(Pedido)
-admin.site.register(ItemPedido)
