6fc540ca8f1a34b8dea2d3f143f646962c6c50c0
Dustyluc123 <149323009+Dustyluc123@users.noreply.github.com>
2025-11-22 17:55:12 -0300

    [FEATURE] - Crie o arquivo `pedidos/urls.py` para gerenciar as rotas específicas do aplicativo 'pedidos'. Defina um `app_name = 'pedidos'` e uma lista `urlpatterns` inicial, que pode estar vazia por enquanto. Lembre-se também de incluir este novo `urls.py` no arquivo de URLs do projeto principal para que as rotas se tornem acessíveis.

---
+++ pedidos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+app_name = 'pedidos'
+
+urlpatterns = [
+    # As rotas específicas do aplicativo 'pedidos' serão adicionadas aqui.
+    # Exemplo:
+    # path('', views.lista_pedidos, name='lista_pedidos'),
+    # path('<int:pk>/', views.detalhe_pedido, name='detalhe_pedido'),
+]
